df3c592949f5c4ba9ce71a5d2d7b37f0b92c5388
omare334 <omareweis123@gmail.com>
2022-12-03 19:14:24 +0000

    made world cup data analysis script and imported data

---
+++ predicting the world cup/world cup data analysis.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read the data from the csv file
+df = pd.read_csv('players_fifa23.csv')
+#read first five rows
+print(df.head())
+#read last five rows
+print(df.tail())
+
+
+
+
+
